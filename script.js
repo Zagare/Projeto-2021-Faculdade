15765461a9ba937acbd8618e2eaaf5a6cb01d13f
Luís Zagare <111555394+Zagare@users.noreply.github.com>
2024-08-22 01:20:16 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+/* COMEÇO slides */
+$("#slideshow > div:gt(0)").hide();
+
+setInterval(function () {
+  $('#slideshow > div:first')
+    .fadeOut(1000)
+    .next()
+    .fadeIn(1000)
+    .end()
+    .appendTo('#slideshow');
+}, 3000);
+/* FIM slides */
+
+/*COMEÇO smooth scroll */
+var $root = $('html, body');
+
+$('a[href^="#"]').click(function () {
+    $root.animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 500);
+
+    return false;
+});
+/*FIM smooth scroll */
